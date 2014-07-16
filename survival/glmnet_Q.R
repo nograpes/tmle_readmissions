e37f50d6773f2d0003920efc4b969f72199a925a
Aman Verma <aman.verma.mtl@gmail.com>
2014-07-16 23:40:38 +0000

    Both of the model building scripts are now added. Summarize models will make some of the nice figures for the survival analysis, including the time-to-event one.

---
+++ survival/glmnet_Q.R
@@ -0,0 +1,20 @@
+suppressPackageStartupMessages(library(survival))
+suppressPackageStartupMessages(library(glmnet))
+suppressPackageStartupMessages(library(doMC))
+registerDoMC(cores=10) # For 10 folds
+
+# arguments<-c('data_dump/disease_ami.object', 'survival/data_dump/glmnet_Q_ami.object')
+arguments<-commandArgs(trailingOnly=TRUE)
+object.file<-arguments[1]
+output.file<-arguments[2]
+load(object.file)
+
+set.seed(1)
+glmnet.predict.outcome <- 
+              cv.glmnet(x=disease.big.matrix, 
+                     y=Surv(disease.df$tte, 
+                            !disease.df$censor),
+					 nfolds=10,
+                     family='cox',
+ 					 parallel=TRUE)
+save(glmnet.predict.outcome,file=output.file)
---
+++ survival/summarize_models.R
@@ -0,0 +1,54 @@
+library(reshape2)
+library(ggplot2)
+# Post-process the Q* file.
+load('survival/data_dump/Q_star_ami.object')
+# Q.star.list epsilon.mat tte.mat
+
+head((table(round(sort(tte.mat[,1])))),10)
+ 
+head((table(round(sort(tte.mat[,2])))),30)
+head((table(round(sort(tte.mat[,3])))),30)
+head((table(round(sort(tte.mat[,4])))),30)
+head((table(round(sort(tte.mat[,5])))),30)
+
+melted.tte <- melt(data.frame(tte.mat, check.names=FALSE), 
+                   id.vars = NULL,
+                   variable.name = "hospital",
+                   value.name="tte")
+
+table()
+
+# All on top.
+ggplot(data=melted.tte, aes(x=tte, colour=hospital)) + 
+  geom_density()
+
+ggplot(data=melted.tte, aes(x=tte, colour=hospital)) + 
+  geom_density() + xlim(c(0,60))
+
+
+?geom_density
+
+
+ggplot(data=melted.tte, aes(x=tte)) + 
+  facet_grid(hospital ~ .) +
+  geom_density() 
+  
+ggplot(data=melted.tte, aes(x=tte,colour=hospital)) + 
+  facet_grid(hospital ~ .) +
+  xlim(c(0,60)) +
+  geom_density() 
+
+ggplot(data=melted.tte, aes(x=tte,colour=hospital)) + 
+  facet_grid(hospital ~ .) +
+  xlim(c(0,120)) +
+  geom_density() 
+
+ggplot(data=melted.tte, aes(x=tte,colour=hospital)) + 
+  facet_grid(hospital ~ .) +
+  geom_density() 
+
+
+hist(tte.mat[,1])
+
+round(tte.mat[,1][tte.mat[,1]<30])
+
