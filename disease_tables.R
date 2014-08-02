38cc22855dd6872eb975d75b9c590ab7cc18a9b4
Aman Verma <aman.verma.mtl@gmail.com>
2014-08-02 15:35:48 +0000

    displayed the new marginal risk for crude readmission risk.

--- disease_tables.R
+++ disease_tables.R
@@ -11,12 +11,20 @@ get.data<-function(prefix) {
   dump.dir<-'data_dump'
   data.files<-c('rf_Q_star_model_', 'disease_','crude_readmissions_risk_')
   files<-paste0('data_dump/', data.files, prefix, '.object')
-  files<-c(files,paste0('survival/data_dump/Q_star_survival_',prefix,'.object'))
+  # files<-c(files,paste0('survival/data_dump/Q_star_survival_',prefix,'.object'))
   e<-new.env()
   for(file in files) load(file,envir=e)
   mget(ls(e),envir=e)
 }
 models<-sapply(prefixes, get.data, simplify=FALSE) # Using sapply here to get the names.
+
+# Reconstruct QAW.
+for (i in seq_along(models)){
+  model=models[[i]]
+  Q <- model$all.rf.Q.by.hosp
+  cols <- match(model$disease.df$hosp,colnames(Q))
+  models[[i]]$QAW <- Q[cbind(seq(length(cols)), cols)]
+}
 
 # Now, make a table for Q*?
 dump.base.stats<-function(disease,los=FALSE) {
@@ -28,34 +36,10 @@ dump.base.stats<-function(disease,los=FALSE) {
   died.during.stay <- models[[disease]] [['died.during.stay']]
   n.died <- c(table(died.during.stay$hosp))
   # Length-of-stay
-  
-  dash.NAs <- function(x){
-    m<-merge(data.frame(hosp=levels(disease.df$hosp)),x,all.x=TRUE)
-    # m[is.na(m$los),'los']<-'-'
-    m
-  }
+  dash.NAs <- function(x)
+    merge(data.frame(hosp=levels(disease.df$hosp)),x,all.x=TRUE)
 
-  if (los) {
-  mean.survived.los <- dash.NAs(aggregate(los~hosp,disease.df,mean))[,'los']
-  mean.died.los <- dash.NAs(aggregate(los~hosp,died.during.stay,mean))[,'los']
-  mean.both.los <- dash.NAs(aggregate(los~hosp,
-                                      rbind(disease.df[c('hosp','los')], 
-                                            died.during.stay[c('hosp','los')]),
-                                      mean)) [,'los']
-  }
-  
-  d = data.frame(admitted=n+n.died, died=n.died, died.prop=n.died/(n+n.died), live.discharge=n, readmitted=crude.readmitted.n, prop=crude.props)
-  if(los) return(cbind(d,data.frame(overall.los=mean.both.los,died.los=mean.died.los,survived.los=mean.survived.los)))
-  d
-}
-
-dump.results.df<-function(disease, model) {
-  # all.Q.by.hosp <- models[[disease]][[paste0('all.',model,'.Q.by.hosp')]]
-  # epsilon <- models[[disease]][[paste0(model,'.epsilons')]]
-  Q.star.by.epsilon <- models[[disease]][[paste0(model,'.Q.star')]]
-  tte <- models[[disease]][['tte.mat']]
-  
-  hazards.mat <- models[[disease]][['hazards.mat']]
+  # Logistic model
   logistic.mat <- models[[disease]][['logistic.mat']]
   base <-  models[[disease]]$base
   
@@ -64,18 +48,74 @@ dump.results.df<-function(disease, model) {
   
   odds <- add.base(logistic.mat)
   colnames(odds) <- c('odds.ratio','odds.ratio.ci.low','odds.ratio.ci.high')
-
-  hazards <- add.base(hazards.mat)
-  colnames(hazards) <- c('hazard.ratio','hazard.ratio.ci.low','hazard.ratio.ci.high')
   
-  data.frame(odds,Q.star=colMeans(Q.star.by.epsilon),
-             hazards,mean.tte=colMeans(tte),median.tte=apply(tte,2,median))
+  data.frame(admitted=n+n.died, 
+             died=n.died, 
+             died.prop=n.died/(n+n.died), 
+             live.discharge=n, 
+             readmitted=crude.readmitted.n, 
+             prop=crude.props, 
+             odds,
+             marginal.risk=models[[disease]]$marginal.risk.by.hosp
+             )
 }
 
-disease.table<-function(disease, models='rf') {
-  model.tables <- lapply(models, dump.results.df, disease=disease)
+# The traditional way is to bound:
+bound <- function (x, bounds=c(0.025,0.975)) 
+{
+  x <- ifelse(x>max(bounds),max(bounds),x)
+  ifelse(x<min(bounds),min(bounds),x)
+}
+
+dump.results.df<-function(disease, bound) {
+  Q.star.by.bound <- models[[disease]]$Q.star.by.bound
+  bounds <- models[[disease]]$bounds
+  Q.star <- Q.star.by.bound[[match(bound, bounds)]]
+  average.risk <- colMeans(Q.star)
+  base <-  models[[disease]]$base
+
+  # Calculate OR for TMLE
+  QAW <- models[[disease]]$QAW
+  mu0 <- average.risk[base]
+  psi <- (average.risk/(1-average.risk)) / (mu0/(1-mu0))
+  g.by.rf <- bound(models[[disease]]$g.by.rf, 
+                   bounds=c(bound,1-bound))
+  Q.by.rf <- models[[disease]]$all.rf.Q.by.hosp
+  hosps <- names(average.risk)
+  disease.df <- models[[disease]]$disease.df
+  
+  base.var.calc <- function(hosp) {
+    g1W <- g.by.rf[,hosp]
+    Q1W <- Q.by.rf[,hosp]
+    Y <- as.numeric(disease.df$day_30_readmit)
+    A<-disease.df$hosp
+    mu = average.risk[hosp]
+    (1/(mu*(1-mu))) * ((((A==hosp) / g1W) * (Y-QAW)) + Q1W)
+  }
+
+  base.var <- sapply(hosps, base.var.calc)
+  compared.var <- base.var - base.var[,base]
+  var.psi <- apply(compared.var,2,var) / nrow(disease.df)
+  
+  odds.func <- function(x) x/(1-x)
+  psi <- odds.func(average.risk) / odds.func(mu0)
+  psi.ci <- exp(log(psi) + t((qnorm(0.975) * c(-1,1)) %*% t(var.psi)))
+  
+  tmle.odds <- cbind(psi,psi.ci)
+  tmle.odds [base,] <- NA
+  colnames(tmle.odds) <- c('or','low.ci','high.ci')
+
+  data.frame(tmle.odds,
+             Q.star=average.risk)
+}
+
+disease.table<-function(disease, bound) {
+  model.tables <- lapply(bound, dump.results.df, disease=disease)
   cbind(dump.base.stats(disease), Reduce(cbind, model.tables))
 }
 
-disease.results.table<-sapply(prefixes, disease.table, simplify=FALSE)
+disease.results.table<-sapply(prefixes, 
+                              disease.table, 
+                              bound=c(10^-1.6,10^-2), 
+                              simplify=FALSE)
 save(disease.results.table,file=output.file)
